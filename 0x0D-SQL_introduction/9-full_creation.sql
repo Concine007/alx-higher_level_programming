1a6bf4d6f3c992b671528b3f046318678db0a283
concine007 <natnaelabebe177@hotmail.com>
2023-08-17 13:42:59 -0700

    0x0D

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
-
+-- a script that creates a table second_table in the database
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- Script should create maximum of 4 records
